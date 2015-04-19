66bd4e7e807022fd26dc8d9245cc5c98d3b6d567
yungsters <yungsters@gmail.com>
2015-04-18 23:48:37 -0700

    Use POJO for channels.

--- src/URTSI.js
+++ src/URTSI.js
@@ -7,20 +7,35 @@ var stringPad = require('./stringPad');
 class URTSI {
   constructor(serialPath, address = 1) {
     invariant(address > 0 && address < 100, 'Invalid address: ' + address);
-    this._address = address;
-    this._path = serialPath;
-    this._serialPort = null;
+
+    var execute = (channel, direction) => getSerialPort(serialPath).then(
+      serialPort => new Promise((resolve, reject) => {
+        serialPort.write(
+          stringPad('' + address, 2) + stringPad('' + channel, 2) + direction,
+          error => error ? reject(error) : resolve()
+        );
+      })
+    );
+
     this._channels = [];
     for (var ii = 1; ii <= 16; ii++) {
-      this._channels.push(new URTSIChannel(this, ii));
+      this._channels.push({
+        down: execute.bind(null, ii, 'D'),
+        stop: execute.bind(null, ii, 'S'),
+        up:   execute.bind(null, ii, 'U'),
+      });
     }
   }
+
   getChannels() {
     return this._channels;
   }
-  execute(channel, direction) {
-    this._serialPort = this._serialPort || new Promise((resolve, reject) => {
-      var serialPort = new SerialPort(this._path, {
+}
+
+var getSerialPort = (serialPorts => function(serialPath) {
+  if (!serialPorts.hasOwnProperty(serialPath)) {
+    serialPorts[serialPath] = new Promise((resolve, reject) => {
+      var serialPort = new SerialPort(serialPath, {
         baudRate: 9600,
         dataBits: 8,
         stopBits: 1,
@@ -29,40 +44,8 @@ class URTSI {
       serialPort.on('open', () => resolve(serialPort));
       serialPort.on('error', reject);
     });
-    return this._serialPort.then(
-      serialPort => new Promise((resolve, reject) => {
-        var command =
-          stringPad('' + this._address, 2) +
-          stringPad('' + channel, 2) +
-          direction;
-
-        serialPort.write(command, error => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve();
-          }
-        });
-      })
-    );
   }
-}
-
-class URTSIChannel {
-  constructor(urtsi, channel) {
-    invariant(channel > 0 && channel < 17, 'Invalid motor channel: ' + channel);
-    this._urtsi = urtsi;
-    this._channel = channel;
-  }
-  up() {
-    return this._urtsi.execute(this._channel, 'U');
-  }
-  down() {
-    return this._urtsi.execute(this._channel, 'D');
-  }
-  stop() {
-    return this._urtsi.execute(this._channel, 'S');
-  }
-}
+  return serialPorts[serialPath];
+})({});
 
 module.exports = URTSI;
