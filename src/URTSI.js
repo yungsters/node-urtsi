ea0298950f93f65fdc7fd6c3764948375575c586
yungsters <yungsters@gmail.com>
2015-04-22 23:39:44 -0700

    Add `getChannel()`, `getChannelGroup()`, and arguments to `getChannels(...)`.

--- src/URTSI.js
+++ src/URTSI.js
@@ -7,10 +7,10 @@ var stringPad = require('./stringPad');
 class URTSI {
   constructor(serialPath) {
     var address = 1; // Address for RS-232 is always 01.
-    var execute = (channel, direction) => getSerialPort(serialPath).then(
+    var execute = (channelID, direction) => getSerialPort(serialPath).then(
       serialPort => new Promise((resolve, reject) => {
         serialPort.write(
-          stringPad('' + address, 2) + stringPad('' + channel, 2) + direction,
+          stringPad('' + address, 2) + stringPad('' + channelID, 2) + direction,
           error => error ? reject(error) : resolve()
         );
       })
@@ -20,16 +20,16 @@ class URTSI {
     var prevStop = Promise.resolve();
 
     this._channels = Array(...Array(16)).map((_, ii) => {
-      var channel = ii + 1;
+      var channelID = ii + 1;
       return {
         up() {
-          return execute(channel, 'U');
+          return execute(channelID, 'U');
         },
         down() {
-          return execute(channel, 'D');
+          return execute(channelID, 'D');
         },
         stop() {
-          var result = prevStop.then(() => execute(channel, 'S'));
+          var result = prevStop.then(() => execute(channelID, 'S'));
           prevStop = prevStop.then(() => new Promise(resolve => {
             setTimeout(resolve, 600);
           }));
@@ -39,8 +39,36 @@ class URTSI {
     });
   }
 
-  getChannels() {
-    return this._channels;
+  getChannel(channelID) {
+    var ii = channelID - 1;
+    invariant(
+      this._channels.hasOwnProperty(ii),
+      'Invalid channel ID: ' + channelID
+    );
+    return this._channels[ii];
+  }
+
+  getChannels(...channelIDs) {
+    if (channelIDs.length) {
+      return channelIDs.map(channelID => this.getChannel(channelID));
+    } else {
+      return this._channels.slice(0);
+    }
+  }
+
+  getChannelGroup(...channelIDs) {
+    var channels = this.getChannels(...channelIDs);
+    return {
+      up() {
+        return Promise.all(channels.map(channel => channel.up()));
+      },
+      down() {
+        return Promise.all(channels.map(channel => channel.down()));
+      },
+      stop() {
+        return Promise.all(channels.map(channel => channel.stop()));
+      }
+    };
   }
 }
 
